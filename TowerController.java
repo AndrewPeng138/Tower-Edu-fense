d2441a7c5ddd3e23199f3303fa700d236611794c
Andrew Peng <andrewpeng2005@gmail.com>
2024-09-04 20:35:40 -0600

    updated tower controller and model stubs

--- TowerController.java
+++ TowerController.java
@@ -1,38 +1,32 @@
 public class TowerController {
+
     /**
      * Gets x coordinate of tower
      * @return x coordinate of tower
      */
-    public int getX(){
-        // TODO: implement method
-        return this.x;
-    }
-    /**
-     * Gets y coordinate of tower
-     * @return y coordinate of tower
-     */
-    public int getY(){
-        // TODO: implement method
-        return this.y;
+    public String getLocation(TowerModel tower) {
+        String location = tower.getX() + "," + tower.getY();
+        return location;
     }
 
     /**
-     * Sets location of tower on game board
+     * Sets location of tower on game board, should only be activated if the tower was selected
      * @param x x-coordinate to be set
      * @param y y-coordinate to be set
      */
-    public void setLocation(int x, int y){
-        // TODO: implement method
-        this.x = x;
-        this.y = y;
+    public void setLocation(TowerModel tower, int x, int y){
+        if(this.isSelected(tower)){
+        tower.setX(x);
+        tower.setY(y);
+        }
     }
 
     /**
      * Checks if tower is selected to be placed on game board
      * @return if button for tower is selected
      */
-    public boolean isSelected(){
-        // TODO: implement method
-        return jButton.isPressed();
+    public boolean isSelected(TowerModel tower){
+        // TODO: need to get more information on how GUI will react to get action of once button is pushed
+        return JButton.isPressed();
     }
 }
--- TowerModel.java
+++ TowerModel.java
@@ -1,14 +1,43 @@
-public class TowerModel {
+public abstract class TowerModel {
     private int damage;
     private String SpecialEffect;
     private int cost;
-    private int range;
+    // No longer having range limits on towers
+    //private int range;
     private int x;
     private int y;
+
     /**
-     * Basic attack from tower onto enemies
+     * Basic attack from tower onto enemies (different for each type of tower)
      */
-    public void Fire(){
-        // TODO: implement method
+    abstract public void Fire();
+    /**
+     * Gets x coordinate of tower
+     * @return x coordinate of tower
+     */
+    public int getX(){
+        return this.x;
+    }
+    /**
+     * Gets y coordinate of tower
+     * @return y coordinate of tower
+     */
+    public int getY(){
+        return this.y;
+    }
+
+    /**
+     * Set the x coordinate of tower
+     * @param value x coordinate to be set
+     */
+    public void setX(int value){
+        this.x = value;
+    }
+    /**
+     * Set the y coordinate of tower
+     * @param value y coordinate to be set
+     */
+    public void setY(int value){
+        this.y = value;
     }
 }
