12784bbcb4d21e4d2547f84b1a76b44e84bf83b1
Andrew Peng <andrewpeng2005@gmail.com>
2024-09-06 11:10:56 -0600

    basic changes to enemy stubs into actual code

--- EnemyController.java
+++ EnemyController.java
@@ -1,36 +1,38 @@
 public class EnemyController {
     /**
      * Sets health of enemy
+     * @param enemy enemy to set health of
      * @param value health value to be set
      */
-    public void setHealth(int value){
-        // TODO: implement method
+    public void setHealth(EnemyModel enemy, int value){
         enemy.health = value;
     }
     /**
      * Sets speed of enemy
+     * @param enemy enemy to set speed of
      * @param value speed value to be set
      */
-    public void setSpeed(int value){
-        // TODO: implement method
+    public void setSpeed(EnemyModel enemy, int value){
         enemy.speed = value;
     }
     /**
      * Sets health of enemy
+     * @param enemy enemy to set damage of
      * @param value health value to be set
      */
-    public void setHealth(int value){
-        // TODO: implement method
-        enemy.damage = value;
+    public void setHealth(EnemyModel enemy, int value){
+        enemy.setHealth(value);
     }
 
     /**
      * Series of action to take place once enemy reaches end of path
      * enemy disappears, user health decreases
+     * @param enemy enemy to set health to 0
+     * @param user user to decrease health
      */
-    public void reachEnd(){
-        // TODO: implement method
+    public void reachEnd(EnemyModel enemy, UserModel user){
+        // TODO: need more refinement on the specific details of what is going to happen after a enemy reaches the end of the path
         enemy.setHealth(0);
-        player.decreaseUserHealth(value);
+        user.decreaseUserHealth(value);
     }
 }
--- EnemyModel.java
+++ EnemyModel.java
@@ -2,12 +2,64 @@ public class EnemyModel {
     private int damage;
     private int health;
     private int speed;
+    private int x;
+    private int y;
 
     /**
      * basic movement by enemy
      */
     public void move(){
-        // TODO: implement method
-        enemy.position() = nextTile;
+        // TODO: this method needs to be more defined by the tile class or another way we need to find the next tile
+        this.position() = nextTile;
+    }
+    /**
+     * Gets x coordinate of enemy
+     * @return x coordinate of enemy
+     */
+    public int getX(){
+        return this.x;
+    }
+    /**
+     * Gets y coordinate of enemy
+     * @return y coordinate of enemy
+     */
+    public int getY(){
+        return this.y;
+    }
+
+    /**
+     * Set the x coordinate of enemy
+     * @param value x coordinate to be set
+     */
+    public void setX(int value){
+        this.x = value;
+    }
+    /**
+     * Set the y coordinate of enemy
+     * @param value y coordinate to be set
+     */
+    public void setY(int value){
+        this.y = value;
+    }
+    /**
+     * Sets health of enemy
+     * @param value health value to be set
+     */
+    public void setHealth(int value){
+        this.health = value;
+    }
+    /**
+     * Sets speed of enemy
+     * @param value speed value to be set
+     */
+    public void setSpeed(int value){
+        this.speed = value;
+    }
+    /**
+     * Sets health of enemy
+     * @param value health value to be set
+     */
+    public void setHealth(int value){
+        this.damage = value;
     }
 }
